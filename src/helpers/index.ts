41409b54cfc06c7b21211c635d37ad9281b8ab3a
AURTeck <112095879+aurteck-codehub@users.noreply.github.com>
2023-12-26 12:45:10 +0500

    adding the helper function to the app for securing the password with node:crypto

---
+++ src/helpers/index.ts
@@ -0,0 +1,9 @@
+import crypto from "node:crypto"
+
+const SECRET:string|any = process.env.APP_SECRET
+
+export const random = () => crypto.randomBytes(128).toString('base64');
+
+export const authentication = (salt: string, password: string) => {
+    return crypto.createHmac('sha256', [salt, password].join('/')).update(SECRET).digest('hex')
+}
